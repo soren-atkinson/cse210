e4b26ea66a616991e1cf4b1de8a32160f7821a12
soren-atkinson <sorenatkinson0905@gmail.com>
2026-09-19 17:03:31 -0600

    Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First Name? ");
+        string fName = Console.ReadLine();
+
+        Console.Write("What is your Last Name? ");
+        string lName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lName}, {fName} {lName}.");
     }
 }
