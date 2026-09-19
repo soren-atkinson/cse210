a59293073b0ed6c6bad93c12bff2e2d75580b8c1
soren-atkinson <sorenatkinson0905@gmail.com>
2026-09-19 17:16:18 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,41 @@
 using System;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade %? ");
+        int grade = Console.ReadLine();
+
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You have an B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You have an C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You have an D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("You have an F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("Better Luck Next Time");
+        }
     }
 }
