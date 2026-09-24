27700ef276fb04a7132ca0cb002425bfd3d7c84f
soren-atkinson <sorenatkinson0905@gmail.com>
2026-09-24 14:14:57 -0600

    Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input = -1;
+
+        while (input != 0)
+        {
+            Console.Write("Add: ");
+            input = int.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+        Console.WriteLine($"Sum is {sum}.");
+        int average = sum / numbers.Count;
+        Console.WriteLine($"Average is {average}.");
+        int largeist = 0;
+        foreach (int number in numbers)
+        {
+            if (number > largeist)
+            {
+                largeist = number;
+            }
+        }
+        Console.WriteLine($"Largeist Number is {largeist}");
     }
 }
