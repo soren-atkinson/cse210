e8ad2b57cde190dc609ab58e04b56d394891070f
soren-atkinson <sorenatkinson0905@gmail.com>
2026-09-24 13:41:39 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+        int guess = -1;
+        string guess_string;
+        while (guess != magic)
+        {
+            Console.Write("What is your guess? ");
+            guess_string = Console.ReadLine();
+            guess = int.Parse(guess_string);
+            if (guess < magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess == magic)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+         }    }
 }
