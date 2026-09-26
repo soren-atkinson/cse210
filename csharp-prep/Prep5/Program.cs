e3eaa300b807126a1f318676986e2b8e7c2b10c0
soren-atkinson <sorenatkinson0905@gmail.com>
2026-09-25 19:44:09 -0600

    Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        
+        int year;
+        PromptUserBirthYear(out year);
+        DisplayResult(name, SquareNumber(number), year);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static void PromptUserBirthYear(out int year)
+    {
+        Console.Write("Please enter the year you were born: ");
+        year = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int x)
+    {
+        x = x * x;
+        return x;
+    }
+
+    static void DisplayResult(string name, int number, int year)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+        int age = 2026 - year;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
     }
 }
